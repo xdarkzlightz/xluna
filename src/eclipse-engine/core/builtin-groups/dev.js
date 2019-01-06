b6c175a3e4295de7738e686c5322340570021d12
xdarkzlightz <darkz66987@protonmail.com>
2019-01-05 22:58:01 -0800

    Removed load command

--- src/eclipse-engine/core/builtin-groups/dev.js
+++ src/eclipse-engine/core/builtin-groups/dev.js
@@ -2,19 +2,6 @@ export const GroupConfig = {
   name: 'dev'
 }
 
-export const load = {
-  config: {
-    args: [
-      {
-        type: 'string',
-        name: 'file'
-      }
-    ],
-    rating: 0
-  },
-  async run (ctx, { file }) {}
-}
-
 export const reload = {
   async run () {}
 }
