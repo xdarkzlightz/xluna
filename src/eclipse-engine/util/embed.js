2e3531d5bc73bee724a19601a1a49824d1d374a0
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-06 06:05:19 -0800

    Added function for creating a help embed

--- src/eclipse-engine/util/embed.js
+++ src/eclipse-engine/util/embed.js
@@ -64,6 +64,20 @@ export function generateCommandHelp (ctx, embed) {
     }*`
   )
 
+  if (cmd.args) {
+    let args = ''
+    cmd.args.forEach(arg => {
+      let options = '\n\n'
+      if (arg.options) {
+        options = `\nOptions: *${arg.options.join(', --')}*`
+      }
+      args += `${arg.name}\nType: *${arg.type}*\nDescription: *${
+        arg.description
+      }*${options}`
+    })
+    embed.addField('Arguments', args)
+  }
+
   if (cmd.command.config.flags) {
     let flags = ''
     cmd.command.config.flags.forEach(flag => {
@@ -79,20 +93,6 @@ export function generateCommandHelp (ctx, embed) {
       }*\n\n`
     })
     embed.addField('flags', flags)
-  }
-
-  if (cmd.args) {
-    let args = ''
-    cmd.args.forEach(arg => {
-      let options = '\n\n'
-      if (arg.options) {
-        options = `\nOptions: *${arg.options.join(', --')}*`
-      }
-      args += `${arg.name}\nType: *${arg.type}*\nDescription: *${
-        arg.description
-      }*${options}`
-    })
-    embed.addField('Arguments', args)
   }
 }
 
@@ -111,4 +111,49 @@ export function generateGroupHelp (ctx, embed) {
     commands += `${cmd.name} ${aliases}\nDescription: ${cmd.description}\n\n`
   })
   embed.addField('commands', commands)
+}
+
+export async function createHelpMessage (ctx, embed) {
+  const app = await ctx.client.fetchApplication()
+  embed.setAuthor(`Bot Help`, app.iconURL)
+  embed.setDescription(
+    `Hello! I am a bot created by ${
+      app.owner.tag
+    }.Currently my only feature is uno but I'll have more soonTM\n` +
+      'Server config: I offer different commands and command flags that allow you to configure your server!\n' +
+      `${
+        ctx.prefix
+      }config (rating) lets you setup a config for the server, ratings: pg, pg13, nsfw.\n` +
+      `${
+        ctx.prefix
+      }config --prefix (prefix) lets you set a prefix for the server\n` +
+      'Command flags: Command flags start with a -- and let you do actions\n' +
+      '--enable-role lets you enable a command for a role, doing --disable-role lets you disable a command for a role\n' +
+      'Flags that are enabled on all commands: --enable-role, --disable-role, --enable-member, --disable-member, --enable-channel, --disable-channel\n' +
+      'Note: If you need to use spaces you have to put the argument in single quotes\n' +
+      `Example: ${ctx.prefix}config --clear-channel 'Test Channel'`
+  )
+  let groups = ''
+  ctx.client.registry.groups.forEach(group => {
+    if (group.devOnly) return
+
+    let commands = ''
+    group.commands.forEach(cmd => {
+      let aliases = ''
+      if (cmd.aliases) {
+        aliases = `(${cmd.aliases.join(', ')})`
+      }
+      commands += `  ${cmd.name} ${aliases} - *${cmd.description}*\n`
+    })
+    groups += `**${group.name}** - ${group.description}:\n${commands}\n`
+  })
+
+  embed.addField('Groups', groups)
+
+  embed.setFooter(
+    `Created by: ${app.owner.tag} | Do ${ctx.prefix}group --h or ${
+      ctx.prefix
+    }command --h to get more info`,
+    app.owner.avatarURL
+  )
 }
