ba11433c6479dcff56458935763ced964c7c12a0
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-06 06:04:55 -0800

    Added descriptions, examples, usages for config

--- src/eclipse-engine/core/builtin-groups/guild.js
+++ src/eclipse-engine/core/builtin-groups/guild.js
@@ -2,7 +2,8 @@ import { setPrefix, addGuild, clear } from '@eclipse/database'
 import { ratings } from '@eclipse/util/database'
 
 export const GroupConfig = {
-  name: 'guild'
+  name: 'guild',
+  description: 'Contains useful server configuration commands'
 }
 
 export const config = {
@@ -18,6 +19,9 @@ export const config = {
     flags: [
       {
         name: `set-prefix`,
+        description: 'Sets the server prefix',
+        usage: 'config --set-prefix (prefix)',
+        example: 'config --set-prefix /',
         run: async (ctx, arg) => {
           if (!ctx.db) {
             return ctx.error(
@@ -34,6 +38,9 @@ export const config = {
       },
       {
         name: `clear-channel`,
+        description: 'Lets you clear the config for a channel',
+        usage: 'config --clear-channel (channel)',
+        example: 'config --clear-channel general',
         run: clear,
         arg: { type: 'channel' },
         default: ctx => {
@@ -42,18 +49,27 @@ export const config = {
       },
       {
         name: `clear-member`,
+        description: 'Lets you clear the config for a member',
+        usage: 'config --clear-member (member)',
+        example: 'config --clear-member xdarkzlightz',
         run: clear,
         arg: { type: 'member' }
       },
       {
         name: `clear-role`,
+        description: 'Lets you clear the config for a role',
+        usage: 'config --clear-role (role)',
+        example: 'config --clear-role role',
         run: clear,
         arg: { type: 'role' },
         default: ctx => {
           return ctx.guild.roles.get(ctx.guild.id)
         }
       }
-    ]
+    ],
+    description: 'Lets you create a server config',
+    usage: 'config (rating)',
+    example: 'config pg'
   },
   async run (ctx, { rating }) {
     const guildExists = ctx.db
