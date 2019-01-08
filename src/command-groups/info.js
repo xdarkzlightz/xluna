fa43619ddcf3654b8098a7f7b95b2d226a6d6b91
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-07 20:20:57 -0800

    Added run functions

--- src/command-groups/info.js
+++ src/command-groups/info.js
@@ -1,4 +1,12 @@
-import { sendHelp, sendInvite } from '@eclipse/info/commands'
+import {
+  sendHelp,
+  sendInvite,
+  sendBotInfo,
+  sendMemberInfo,
+  sendChannelInfo,
+  sendServerInfo,
+  sendRoleInfo
+} from '@info/commands'
 
 export const GroupConfig = {
   name: 'info',
@@ -20,4 +28,51 @@ export const invite = {
     usage: 'invite'
   },
   run: sendInvite
+}
+
+export const botinfo = {
+  config: {
+    description: 'Get info on the bot!',
+    usage: 'botinfo'
+  },
+  run: sendBotInfo
+}
+
+export const serverinfo = {
+  config: {
+    description: 'Get info on the server',
+    usage: 'serverinfo'
+  },
+  run: sendServerInfo
+}
+
+export const memberinfo = {
+  config: {
+    aliases: ['whois'],
+    description:
+      "Get info on a member (If you don't specify a member it will target you)",
+    usage: 'memberinfo (member)',
+    example: 'memberinfo xdarkzlightz'
+  },
+  run: sendMemberInfo
+}
+
+export const channelinfo = {
+  config: {
+    description:
+      "Get info on a channel (If you don't specify a channel it will target the channel you're in",
+    usage: 'channelinfo (channel)',
+    example: 'channelinfo general'
+  },
+  run: sendChannelInfo
+}
+
+export const roleinfo = {
+  config: {
+    description:
+      "Get info on a role (If you don't specify a role then it will target the everyone role)",
+    usage: 'roleinfo (role)',
+    example: "roleinfo 'cool people'"
+  },
+  run: sendRoleInfo
 }
