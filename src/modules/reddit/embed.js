f54ba0170a2ab853df2f6640e0f9aa1d780c48bf
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-09 03:43:57 -0800

    Added til embed function

--- src/modules/reddit/embed.js
+++ src/modules/reddit/embed.js
@@ -1,3 +1,4 @@
+// FIND BETTER WAY TO EMBED/CHECK FOR IMAGES
 export async function embedPost (ctx, post, embed) {
   embed
     .setColor(0xd82162)
@@ -6,10 +7,10 @@ export async function embedPost (ctx, post, embed) {
   if (post.url) {
     if (post.selftext) {
       embed.setDescription(
-        `${post.selftext}\n[Image not loading? Click here](${post.url})`
+        `${post.selftext}\n[Click here to see the source!](${post.url})`
       )
     } else {
-      embed.setDescription(`[Image not loading? Click here](${post.url})`)
+      embed.setDescription(`[Click here to see the source!](${post.url})`)
     }
     embed.setAuthor(
       post.subreddit_name_prefixed,
@@ -17,6 +18,34 @@ export async function embedPost (ctx, post, embed) {
       post.url
     )
     embed.setImage(post.url)
+  } else {
+    if (post.selftext) embed.setDescription(post.selftext)
+    embed.setAuthor(
+      post.subreddit_name_prefixed,
+      'https://image.ibb.co/eRrdV9/icons8-reddit-50.png'
+    )
+  }
+}
+
+export function embedTIL (ctx, post, embed) {
+  embed
+    .setColor(0xd82162)
+    .setTitle(post.title)
+    .setFooter(`Posted by: u/${post.author.name}`)
+  if (post.url) {
+    if (post.selftext) {
+      embed.setDescription(
+        `${post.selftext}\n[Click here to see the source!](${post.url})`
+      )
+    } else {
+      embed.setDescription(`[Click here to see the source!](${post.url})`)
+    }
+    embed.setAuthor(
+      post.subreddit_name_prefixed,
+      'https://image.ibb.co/eRrdV9/icons8-reddit-50.png',
+      post.url
+    )
+    embed.setThumbnail(post.thumbnail)
   } else {
     if (post.selftext) embed.setDescription(post.selftext)
     embed.setAuthor(
