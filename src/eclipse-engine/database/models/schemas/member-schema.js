2f798d4a6a1a0b940ee35d258f933bcf7fd9fdde
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-13 04:09:35 -0800

    Added modlogs to schema

--- src/eclipse-engine/database/models/schemas/member-schema.js
+++ src/eclipse-engine/database/models/schemas/member-schema.js
@@ -2,13 +2,15 @@ import { Schema as _Schema } from 'mongoose'
 
 import GroupSchema from './group-schema'
 import WarningSchema from './warning-schema'
+import LogSchema from './log-schema'
 
 const Schema = _Schema
 
 const MemberSchema = new Schema({
   id: String,
   groups: [GroupSchema],
-  warnings: [WarningSchema]
+  warnings: [WarningSchema],
+  modLogs: [LogSchema]
 })
 
 export default MemberSchema
