6a994484ff1899f7738c8ebbaff74e6aedc0c06f
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-12 22:48:35 -0800

    Added tags option to schema (In later versions of the engine this will be removed)

--- src/eclipse-engine/database/models/schemas/config-schema.js
+++ src/eclipse-engine/database/models/schemas/config-schema.js
@@ -1,10 +1,13 @@
 import { Schema as _Schema } from 'mongoose'
 
+import TagSchema from './tag-schema'
+
 const Schema = _Schema
 
 const ConfigSchema = new Schema({
   prefix: String,
-  rating: Number
+  rating: Number,
+  tags: [TagSchema]
 })
 
 export default ConfigSchema
