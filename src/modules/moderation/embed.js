33e79f2f949ac5068f61c454b7a483c645d95e53
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-13 04:10:29 -0800

    Added embedLog function

--- src/modules/moderation/embed.js
+++ src/modules/moderation/embed.js
@@ -21,3 +21,26 @@ export async function embedWarnings (embed, member, ctx) {
     )
   })
 }
+
+export async function embedLogs (embed, member, ctx) {
+  embed.setAuthor(`${member.user.tag} (${member.id})`, member.user.avatarURL)
+  embed.setColor(0x8b89e7)
+  const dbMember = getMember(member.id, ctx.db)
+  if (!dbMember.modLogs.length) {
+    return embed.setDescription(`${member.user.tag} has no logs!`)
+  }
+
+  await asyncForEach(dbMember.modLogs, async log => {
+    const mod = await ctx.client.fetchUser(log.modID)
+    const pos = dbMember.modLogs.indexOf(log)
+
+    const reason = log.reason ? log.reason : 'N/A'
+
+    embed.addField(
+      `Log #${pos + 1}`,
+      `**${log.action} by:** ${mod.tag} (${mod.id})\n**${log.action} on:** ${
+        log.timestamp
+      }\n**Reason:** ${reason}`
+    )
+  })
+}
