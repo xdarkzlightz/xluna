d5986fc7415605c51cec93613217b292e58486e4
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-14 01:07:32 -0800

    Added welcome/leave configs and a roleID prop

--- src/eclipse-engine/database/models/schemas/config-schema.js
+++ src/eclipse-engine/database/models/schemas/config-schema.js
@@ -2,14 +2,19 @@ import { Schema as _Schema } from 'mongoose'
 
 import TagSchema from './tag-schema'
 import LoggerConfigSchema from './logger-config-schema'
+import WelcomeConfigSchema from './welcome-config-schema'
+import LeaveConfigSchema from './leave-config-schema'
 
 const Schema = _Schema
 
 const ConfigSchema = new Schema({
   prefix: String,
   rating: Number,
+  roleID: String,
   tags: [TagSchema],
-  logger: LoggerConfigSchema
+  logger: LoggerConfigSchema,
+  welcome: WelcomeConfigSchema,
+  leave: LeaveConfigSchema
 })
 
 export default ConfigSchema
