bca9ef8120f75dafb5f72b073347714be5abafcd
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-14 01:06:04 -0800

    Added auto role and join/leave messages

--- src/bot.js
+++ src/bot.js
@@ -80,11 +80,29 @@ client.on('ready', () => {
 
 client.on('guildMemberAdd', async member => {
   const db = await Guild.findOne({ id: member.guild.id })
+  if (db.config.welcome) {
+    const channel = member.guild.channels.get(db.config.welcome.channelID)
+    if (channel) channel.send(`<@${member.id}>, ${db.config.welcome.body}`)
+  }
+
+  if (db.config.roleID) {
+    const role = member.guild.roles.get(db.config.roleID)
+    if (role) await member.addRole(role)
+  }
+
   const dbMember = findID(db.members, member.id)
 
-  if (!member) return
+  if (!dbMember) return
 
   if (dbMember.nickname) member.setNickname(dbMember.nickname)
+})
+
+client.on('guildMemberRemove', async member => {
+  const db = await Guild.findOne({ id: member.guild.id })
+  if (db.config.leave) {
+    const channel = member.guild.channels.get(db.config.leave.channelID)
+    if (channel) channel.send(`<@${member.id}>, ${db.config.leave.body}`)
+  }
 })
 
 client.on('channelCreate', handleChannelCreate)
