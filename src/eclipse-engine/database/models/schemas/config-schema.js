6c540cf994dd215517c5d233c4a07a0ace39c8dd
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-13 22:03:15 -0800

    Added logger config schema

--- src/eclipse-engine/database/models/schemas/config-schema.js
+++ src/eclipse-engine/database/models/schemas/config-schema.js
@@ -1,13 +1,15 @@
 import { Schema as _Schema } from 'mongoose'
 
 import TagSchema from './tag-schema'
+import LoggerConfigSchema from './logger-config-schema'
 
 const Schema = _Schema
 
 const ConfigSchema = new Schema({
   prefix: String,
   rating: Number,
-  tags: [TagSchema]
+  tags: [TagSchema],
+  logger: LoggerConfigSchema
 })
 
 export default ConfigSchema
