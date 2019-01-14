49a32b8deb70385ed950db6a1f12a0b573641b2e
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-14 01:09:05 -0800

    Added command run functions

--- src/modules/serverutil/commands.js
+++ src/modules/serverutil/commands.js
@@ -1,4 +1,12 @@
-import { setChannel, updateLoggerConfig } from '@serverutil/serverutil'
+import {
+  setChannel,
+  updateLoggerConfig,
+  setWelcomeChannel,
+  setLeaveChannel,
+  setLeaveMessage,
+  setWelcomeMessage,
+  setAutoRole
+} from '@serverutil/serverutil'
 
 export async function setLoggingChannel (ctx, channel) {
   await setChannel(channel, ctx.db)
@@ -10,4 +18,32 @@ export async function updateLogger (ctx) {
   if (!updated) ctx.error("A logging channel isn't set!")
 
   ctx.say('Logger updated!')
+}
+
+export async function updateWelcomeChannel (ctx, channel) {
+  await setWelcomeChannel(channel, ctx.db)
+  ctx.success(`Set welcome channel to: ${channel.name}`)
+}
+
+export async function updateLeaveChannel (ctx, channel) {
+  await setLeaveChannel(channel, ctx.db)
+  ctx.success(`Set leave channel to: ${channel.name}`)
+}
+
+export async function updateWelcomeMessage (ctx, message) {
+  await setWelcomeMessage(message, ctx.db)
+
+  ctx.success(`Setted welcomed message`)
+}
+
+export async function updateLeaveMessage (ctx, message) {
+  await setLeaveMessage(message, ctx.db)
+
+  ctx.success(`Setted leave message`)
+}
+
+export async function updateJoinRole (ctx, role) {
+  await setAutoRole(role, ctx.db)
+
+  ctx.success(`Join role set`)
 }
