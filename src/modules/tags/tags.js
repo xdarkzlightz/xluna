569520cded949bf5b6fadff1af91c2fd80516ee3
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-18 01:02:20 -0800

    Fixed tags

--- src/modules/tags/tags.js
+++ src/modules/tags/tags.js
@@ -1,20 +1,23 @@
 import { removeFromArray } from '@eclipse/util/array'
 
 export function getTag (name, ctx) {
-  const db = ctx.guild.db.data
-  const tag = db.data.config.tags.find(tag => tag.name === name)
+  const db = ctx.guild.db
+  if (!db.config.tags) return false
+
+  const tag = db.config.tags.find(tag => tag.name === name)
   if (!tag) return false
   return tag
 }
 
 export async function saveTag (tag, ctx) {
-  const db = ctx.guild.db.data
+  const db = ctx.guild.db
+  if (!db.config.tags) db.config.tags = []
   db.config.tags.push(tag)
-  ctx.db.save(db)
+  ctx.db.save(db.data)
 }
 
 export async function removeTag (tag, ctx) {
-  const db = ctx.guild.db.data
+  const db = ctx.guild.db
   removeFromArray(db.config.tags, tag)
-  ctx.db.save(db)
+  ctx.db.save(db.data)
 }
