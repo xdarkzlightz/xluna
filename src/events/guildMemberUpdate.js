7cfdca238b668c8bec1eb7a8909e2261a219c3cf
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-19 14:09:43 -0800

    fixed a slight bug

--- src/events/guildMemberUpdate.js
+++ src/events/guildMemberUpdate.js
@@ -1,6 +1,7 @@
 import { handleMemberUpdate } from '@serverutil/events'
 
 module.exports = (client, oldMember, member) => {
+  if (!member.guild) return
   const db = client.db.guilds.get(member.guild.id)
   if (!db || !db.config.logger) return
   handleMemberUpdate(oldMember, member, db)
