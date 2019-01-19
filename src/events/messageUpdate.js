aaa1086ecb126ca40971379377ded32b5059addb
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-19 14:19:51 -0800

    Another small bug fix

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -1,6 +1,7 @@
 import { handleMessageUpdate } from '@serverutil/events'
 
 module.exports = (client, oldMessage, newMessage) => {
+  if (!newMessage.guild) return
   const db = client.db.guilds.get(newMessage.guild.id)
   if (!db || !db.config.logger) return
   handleMessageUpdate(oldMessage, newMessage, db)
