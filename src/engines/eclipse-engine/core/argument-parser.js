f602102baa5eac5712ad34714304d5fc487df36f
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-27 19:55:21 -0800

    Improved argument parser, uses a lot less code

--- src/engines/eclipse-engine/core/argument-parser.js
+++ src/engines/eclipse-engine/core/argument-parser.js
@@ -1,4 +1,5 @@
 import EclipseError from '@engines/eclipse/error/EclipseError'
+import { hasPermission, dev } from '../util/other'
 
 /*
   Add clear and status command flags
@@ -17,18 +18,18 @@ class ArgumentParser {
   // Parses args from an argument array
   async parseArgs (cmd, argsArray, ctx, parsed = {}, pass = 0) {
     if (!argsArray.length) {
+      if (!argsArray[pass] && !pass) {
+        throw new EclipseError(
+          { type: 'friendly' },
+          `You did not specify a ${cmd.args[0].name}`
+        )
+      }
+
       return parsed
     } else {
       const arg = cmd.args[pass]
 
       if (argsArray.length === 0 && arg) {
-        throw new EclipseError(
-          { type: 'friendly' },
-          `You did not specify a ${arg.name}`
-        )
-      }
-
-      if (!argsArray[pass]) {
         throw new EclipseError(
           { type: 'friendly' },
           `You did not specify a ${arg.name}`
@@ -53,66 +54,33 @@ class ArgumentParser {
       parsed[arg.name] = obj
 
       argsArray.shift()
-      return this.parseArgs(cmd, argsArray, ctx, parsed, pass++)
+      return this.parseArgs(cmd, argsArray, ctx, parsed, (pass += 1))
     }
   }
 
   // Parses a primitve from an argument
   parsePrimitives (type, arg) {
-    let parsedPrimitive
-    if (type === 'string') {
-      parsedPrimitive = arg
-    } else if (type === 'number') {
-      let num = parseInt(arg, 10)
-      if (!isNaN(num)) {
-        parsedPrimitive = num
-      } else {
-        throw new EclipseError(
-          { type: 'friendly' },
-          `Value provided is not a number`
-        )
-      }
-    } else if (type === 'boolean') {
-      parsedPrimitive = arg === 'true'
-    }
-    return parsedPrimitive
+    if (type === 'string') return arg
+    if (type === 'number') {
+      const num = parseInt(arg, 10)
+      if (!isNaN(num)) return num
+      throw new EclipseError(
+        { type: 'friendly' },
+        `Value provided is not a number`
+      )
+    } else if (type === 'boolean') return arg === 'true'
   }
 
   // Returns a eclipse type from an argument
   async parseDiscordTypes (type, arg, ctx) {
-    if (type === 'member') {
-      const type = await this.findDiscordType(
-        'members',
-        arg,
-        ctx,
-        member =>
-          member.id === arg ||
-          member.user.username === arg ||
-          member.user.tag === arg
-      )
-      return type
-    } else if (type === 'role') {
-      const type = await this.findDiscordType(
-        'roles',
-        arg,
-        ctx,
-        role => role.id === arg || role.name === arg
-      )
-      return type
-    } else if (type === 'channel') {
-      const type = await this.findDiscordType(
-        'channels',
-        arg,
-        ctx,
-        channel => channel.id === arg || channel.name === arg
-      )
-      return type
+    const cbs = {
+      roles: r => r.id === arg || r.name === arg,
+      members: m =>
+        m.id === arg || m.user.username === arg || m.user.tag === arg,
+      channels: c => c.id === arg || c.name === arg
     }
-  }
 
-  // Parses a discord type from an argument
-  async findDiscordType (type, arg, ctx, callback) {
-    let obj = await ctx.guild[type].find(val => callback(val))
+    let obj = await ctx.guild[type].find(val => cbs[type](val))
     if (!obj) obj = ctx.msg.mentions[type].first()
     if (!obj) {
       throw new EclipseError(
@@ -125,73 +93,16 @@ class ArgumentParser {
 
   // Parses command flags
   async parseFlags (cmd, args, ctx) {
-    this.logger.debug(`[Argument-Parser]: Parsing flag: ${args[0]}`)
-
     if (!args[0] || !args[0].startsWith('--')) return
-    let flag = cmd.flags.get(args[0].substring(2, args[0].length))
-    if (!flag) flag = cmd.flagAliases.get(args[0].substring(2, args[0].length))
+    const arg = args[0].substring(2, args[0].length)
+    let flag = cmd.flags.get(arg) || cmd.flagAliases.get(arg)
     if (!flag) return true
 
     this.logger.debug(`[Argument-Parser]: Found flag: ${args[0]}`)
-    if (flag.devOnly && ctx.client.devs.indexOf(ctx.author.id) === -1) return
-
-    if (flag.memberPermissions) {
-      let err
-      flag.memberPermissions.forEach(perm => {
-        if (err) return
-        if (!ctx.member.hasPermission(perm)) {
-          err = true
-          return ctx.error(
-            `Could not run flag! You're missing permission: ${perm.toLowerCase()}`
-          )
-        }
-      })
-      if (err) return true
-    }
-
-    return this.runFlag(flag, ctx, args)
-  }
-
-  // Runs a command flag
-  async runFlag (flag, ctx, args) {
-    let obj = {}
-    if (flag.arg) obj = await this.parseFlagArgs(flag, ctx, args)
-    if (flag.args) {
-      args.shift()
-      obj = await this.parseArgs(flag, args, ctx)
-    }
-    if (obj === true) return obj
-
-    await flag.run(ctx, obj)
+    if (flag.devOnly && !dev(ctx)) return true
+    if (!hasPermission(flag)) return true
+    await flag.run(ctx, await this.parseFlagArgs(flag, ctx, args))
     return true
-  }
-
-  async parseFlagArgs (flag, ctx, args) {
-    args = this.getArgs(args.join(' '))
-
-    let arg = args[1]
-    if (!arg) {
-      if (flag.default) {
-        this.logger.debug('[Argument-Parser]: Resorting to default value')
-        return flag.default(ctx)
-      } else {
-        ctx.error(`Missing ${flag.arg.name} argument`)
-        return true
-      }
-    } else if (!flag.args) {
-      this.logger.debug(
-        `[Argument-Parser]: Parsing discord type for ${flag.arg.type}`
-      )
-      if (
-        flag.arg.type === 'channel' ||
-        flag.arg.type === 'member' ||
-        flag.arg.type === 'role'
-      ) {
-        return this.parseDiscordTypes(flag.arg.type, arg, ctx)
-      } else {
-        return this.parsePrimitives(flag.arg.type, arg)
-      }
-    }
   }
 
   // Borrowed from https://github.com/Shinobu1337/discord-command-parser/
