000bbd65021b9caf3d42c090f78471fc812c5ae5
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-27 23:44:03 -0800

    Completed flag class

--- src/engines/eclipse-engine/command/flag.js
+++ src/engines/eclipse-engine/command/flag.js
@@ -1,5 +1,17 @@
-export class Flag {
-  constructor (options) {
-    this.name = options.name
+export default class Flag {
+  constructor (name, options) {
+    this.name = name
+    this.description = options.description
+    this.example = options.example
+    this.usage = options.usage
+    this.notes = options.notes
+
+    this.args = options.args
+    this.default = options.default
+    this.run = options.run
+  }
+
+  async run (ctx) {
+    await this.run(ctx)
   }
 }
