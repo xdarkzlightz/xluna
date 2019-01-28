12437c61a33cbd1481f3979cd5f0309660badcb3
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-27 19:56:16 -0800

    Added hasPermission function and a function for checking for devs

--- src/engines/eclipse-engine/util/other.js
+++ src/engines/eclipse-engine/util/other.js
@@ -22,3 +22,26 @@ export function checkForMemberPerms ({ cmd, member }) {
   })
   return response
 }
+
+export function hasPermission (obj, ctx) {
+  if (obj.memberPermissions) {
+    const { perm, missingPerm } = checkForMemberPerms(ctx)
+    if (!perm) {
+      ctx.say(`You're missing permission: ${missingPerm.toLowerCase()}`)
+      return false
+    }
+  } else if (obj.clientPermissions) {
+    const { perm, missingPerm } = checkForClientPerms(ctx)
+    if (!perm) {
+      ctx.say(`I'm missing permission: ${missingPerm.toLowerCase()}`)
+      return false
+    } else {
+      return true
+    }
+  }
+}
+
+export function dev (ctx) {
+  if (ctx.client.devs.indexOf(ctx.author.id) === -1) return false
+  return true
+}
