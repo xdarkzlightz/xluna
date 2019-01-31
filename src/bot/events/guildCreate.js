4b3ed1ef4e107a6ce13ada0df7bf496c686b539c
xdarkzlightz <xdarkzdev@gmail.com>
2019-01-30 18:44:49 -0800

    Now creates a config on join

--- src/bot/events/guildCreate.js
+++ src/bot/events/guildCreate.js
@@ -4,6 +4,9 @@ import { createJoinEmbed } from '@engines/eclipse/util/embed'
 
 module.exports = async (client, guild) => {
   try {
+    const db = client.db.newGuild(client.prefix, guild.id)
+    await db.save()
+
     client.logger.info(`Joined guild: ${guild.name} (${guild.id})`)
 
     const embed = new RichEmbed().setColor(0x5742f7)
