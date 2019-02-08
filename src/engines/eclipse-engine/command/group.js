8e9d110b84ed76fa06c35468b1fc0b8d5cbb5934
xdarkzlightz <xdarkzdev@gmail.com>
2019-02-08 02:02:10 -0800

    Fixed a small bug with the group help message

--- src/engines/eclipse-engine/command/group.js
+++ src/engines/eclipse-engine/command/group.js
@@ -207,7 +207,7 @@ class Group {
 
   sendHelpMessage (ctx) {
     const embed = new RichEmbed().setColor(0x4286f4)
-    createGroupHelp(ctx, embed)
+    createGroupHelp(ctx.prefix, ctx, embed)
     ctx.say(embed)
   }
 
