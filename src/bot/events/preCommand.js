2fef0acd57a14b7160a7a03da280ae56b2b627ce
xdarkzlightz <xdarkzdev@gmail.com>
2019-02-08 02:34:40 -0800

    Added error handling

--- src/bot/events/preCommand.js
+++ src/bot/events/preCommand.js
@@ -1,47 +1,50 @@
 import { levelUpReady, randomEXP } from '@modules/levels/level'
 
 module.exports = async (client, ctx) => {
-  if (!ctx.guild) return
-  const member = ctx.member.db
-  const channel = ctx.channel.db
+  try {
+    if (!ctx.guild) return
+    const member = ctx.member.db
+    const channel = ctx.channel.db
 
-  let leveledUp = false
-  let readyToLevel = levelUpReady(member.level, member.exp)
-  if (readyToLevel) {
-    leveledUp = true
-    await member.update(m => (m.level += 1))
-    if (channel.expEnabled !== false) {
-      ctx
-        .say(
+    let leveledUp = false
+    let readyToLevel = levelUpReady(member.level, member.exp)
+    if (readyToLevel) {
+      leveledUp = true
+      await member.update(m => (m.level += 1))
+      if (channel.expEnabled !== false) {
+        ctx.say(
           `<@${ctx.member.id}> has reached server level ${member.data.level}`
         )
-        .catch()
+      }
     }
-  }
 
-  let user = ctx.author.db
+    let user = ctx.author.db
 
-  readyToLevel = levelUpReady(user.profile.level, user.profile.exp)
-  if (readyToLevel) {
-    leveledUp = true
-    await user.update(u => (u.profile.level += 1))
-    if (!channel || channel.data.expEnabled !== false) {
-      ctx
-        .say(
+    readyToLevel = levelUpReady(user.profile.level, user.profile.exp)
+    if (readyToLevel) {
+      leveledUp = true
+      await user.update(u => (u.profile.level += 1))
+      if (!channel || channel.data.expEnabled !== false) {
+        ctx.say(
           `<@${ctx.member.id}> has reached global level ${user.profile.level}`
         )
-        .catch()
+      }
     }
-  }
-  if (!ctx.client.levelCooldowns.has(ctx.author.id)) {
-    const exp = randomEXP(10, 50)
-    if (!channel || (channel.data.expEnabled !== false && !leveledUp)) {
-      await member.update(m => (m.exp += exp))
-      await user.update(u => (u.profile.exp += exp))
+    if (!ctx.client.levelCooldowns.has(ctx.author.id)) {
+      const exp = randomEXP(10, 50)
+      if (!channel || (channel.data.expEnabled !== false && !leveledUp)) {
+        await member.update(m => (m.exp += exp))
+        await user.update(u => (u.profile.exp += exp))
+      }
+      ctx.client.levelCooldowns.set(ctx.author.id, ctx.author)
+      setTimeout(() => {
+        ctx.client.levelCooldowns.delete(ctx.author.id)
+      }, 60 * 1000)
     }
-    ctx.client.levelCooldowns.set(ctx.author.id, ctx.author)
-    setTimeout(() => {
-      ctx.client.levelCooldowns.delete(ctx.author.id)
-    }, 60 * 1000)
+  } catch (e) {
+    if (e.message === 'DiscordAPIError: Missing Permissions') return
+    client.logger.debug(
+      `[Pre-Command]: Error: ${e.message}\nStack Trace: ${e.stack}`
+    )
   }
 }
